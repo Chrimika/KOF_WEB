b81677da708bcb74d0fefc9a12d97f23a6f34039
ruxlsr <russelsonwa1@gmail.com>
2025-04-23 19:42:40 +0100

    to the festival component

---
+++ src/app/actualite/font.ts
@@ -0,0 +1,9 @@
+import localFont from "next/font/local";
+
+export const rockwell = localFont({
+  src: "../../../public/fonts/Rockwell-ExtraBold.otf",
+  weight: "800",
+  style: "normal",
+  display: "swap",
+  variable: "--font-rockwell",
+});
