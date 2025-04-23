e64959a76c2f22e0fbf143a8a4783394bd1b56d0
ruxlsr <russelsonwa1@gmail.com>
2025-04-23 17:38:37 +0100

    basic subheader

---
+++ src/app/actualite/components/tentacules/subHeader.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Title } from "../title";
+
+type Props = {};
+
+const SubHeader = (props: Props) => {
+  return (
+    <div className="relative mt-[400px]  w-screen h-fit">
+      {/* Tentacule gauche, à gauche */}
+      <img
+        src="/actualite/left_tentacule.png"
+        alt="tentacule gauche"
+        className="absolute left-[-100px] bottom-[-100] z-0 w-[600px] h-[248.99px] "
+      />
+
+      {/* Tentacule du milieu, au centre */}
+      <img
+        src="/actualite/midle_tentacule.png"
+        alt="tentacule milieu"
+        className="absolute left-1/2 bottom-[-400px] transform  z-0 w-[283px] h-[594px] "
+      />
+
+      {/* Tentacule droite, à droite */}
+      <img
+        src="/actualite/right_tentacule.png"
+        alt="tentacule droite"
+        className="absolute right-0 bottom-[-100px] z-0 w-[376px] h-[248.99px] "
+      />
+
+      <Title />
+    </div>
+  );
+};
+
+export default SubHeader;
