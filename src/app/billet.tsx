8e8dc4033816fe8a3d16170adb88f661c81ad4bd
Chrimika <mbachristian58@gmail.com>
2025-05-08 22:18:32 +0100

    partners page, Billets, CountDown

---
+++ src/app/billet.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Billet() {
+  return (
+    <div style={{
+        flex: 0.8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '40vh',
+        backgroundImage: `url("/assets/images/billet.png")`,
+        backgroundPosition:'center',
+        backgroundRepeat:'no-repeat',
+        backgroundSize:'contain',
+        width:'100%',
+        margin:'0 auto'
+      }}>
+
+      </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,14 @@
+import Billet from "./billet";
+import FestivalCountdown from "./festival";
 import FootHero from "./foot-hero";
+import Galery from "./galery";
 import Header from "./header";
 import Hero from "./hero";
 import Info from "./info";
 import Home from "./page";
 import Paragraphe from "./paragraphe";
+import Partners from "./partners";
+import Countdown from "./Rebours";
 import Scan from "./Scan";
 import App from "./Scan";
 
@@ -11,18 +16,30 @@ export default function RootLayout() {
   return (
     <html>
       <body style={{padding:0,margin:0,display:'flex',flexDirection:'column'}}>
-      <div style={{display:'flex',flexDirection:'column',height:'100vh' }}>
-        <Header />
-        <Hero />
-        <Paragraphe />
-        <FootHero />
-      </div>
-      <div style={{display:'flex',height:'100vh'}}>
-        <Info />
-      </div>
-      {/* <div style={{display:'flex',height:'100vh'}}>
-        <Info />
-      </div> */}
+        <div style={{display:'flex',flexDirection:'column',height:'100vh' }}>
+          <Header />
+          <Hero />
+          <Paragraphe />
+          <FootHero />
+        </div>
+        <div style={{display:'flex',height:'100vh'}}>
+          <Info />
+        </div>
+        <div style={{display:'flex',height:'100vh'}}>
+          <Galery />
+        </div>
+        <div style={{display:'flex',height:'100vh'}}>
+          <Partners />
+        </div>
+        <div style={{display:'flex',height:'50vh'}}>
+          <Billet />
+        </div>
+        <div style={{display:'flex',height:'20vh'}}>
+          <Countdown />
+        </div>
+        <div style={{display:'flex',height:'20vh'}}>
+          <FestivalCountdown />
+        </div>
       </body>
     </html>
   );
